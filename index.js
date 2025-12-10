6ed92e433e060ac9dd7e7b3de97da972708bf2c5
Van Tam Pham <pvantam2003@gmail.com>
2025-12-10 10:38:30 +0200

    Fix CORS configuration for production

--- index.js
+++ index.js
@@ -17,7 +17,12 @@ const ownerDeleteRoutes = require('./DeleteData/owner')
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}))
 
 require('dotenv').config()
 connectDB()
